97692359c62423f2d3a84368a6655f042dc3b9c7
Nery Jr <cneryjr@gmail.com>
2015-10-04 17:44:41 -0300

    Full version 0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # jsonfs
+
+Handles JSON file directly through methods to get, set, remove, getAll and save.
+
+## Installation
+
+  npm install jsonfs --save
+
+## Usage
+
+
+## Release History
+
+* 0.1.0 October/2015 - Initial release
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var _ = require('lodash');
+var fs = require('fs');
+
+
+var Store = function(nome) {
+	this.nome = nome || "package.json";
+	this.path = path.join(process.cwd(), this.nome);
+	this.json = Store.readJSON(this.path);
+};
+
+Store.readJSON = function(path) {
+    if (fs.existsSync(path)) {
+        return JSON.parse(fs.readFileSync(path, 'utf8'));
+    }
+    return {};
+};
+
+Store.prototype = {
+	save: function() {
+		fs.writeFileSync(this.path, JSON.stringify(this.json, null, '  '));
+	},
+
+	set: function(key, value) {
+		// this.json[key] = value;
+
+		 if (_.isObject(key)) {
+		    this.json = _.extend(this.json, key);
+		} else {
+			var obj = this.json;
+			var keys = key.split(".");
+			var lastkey = keys.pop();
+
+			keys.map(function(k){
+			    obj = (obj[k] || (obj[k] = {}));
+			});
+			obj[lastkey] = value;	
+		}
+
+		this.save();
+	},
+
+	get: function(key) {
+		var obj = this.json;
+
+		key.split(".").map(function(k){
+		    if (obj !== undefined) 
+		        obj = obj[k];
+		});
+
+		return obj;
+	},
+
+	getAll: function() {
+		return this.json;
+	},
+
+	remove: function(key) {
+		var obj = this.json;
+		var keys = key.split(".");
+		var lastkey = keys.pop();
+
+		keys.map(function(k){
+		    obj = (obj[k] || (obj[k] = {}));
+		});
+
+		delete obj[lastkey];
+
+		this.save();
+	}
+};
+
+module.exports = Store;
